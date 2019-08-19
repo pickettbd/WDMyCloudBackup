d47b26ccf9115e8835efdb7350055db91e2f05c3
Brandon Pickett <pickettbd@gmail.com>
2019-08-19 09:50:22 -0400

    bug fixes

--- remoteCheck.sh
+++ remoteCheck.sh
@@ -16,7 +16,7 @@ mycloud_error()
 	printf "%s\t%s\n%s\n" \
 		`date '+%Y%m%d-%H:%M:%S'` \
 		"Could not SSH (presumabely the key was de-authorized and we were prompted for a password instead)." \
-		"cat ~/.ssh/id_rsa.pub | ssh "${MYCLOUD}" 'cat - >> /home/root/.ssh/authorized_keys'" \
+		"cat ~/.ssh/id_rsa.pub | ssh \"${MYCLOUD}\" 'cat - >> /home/root/.ssh/authorized_keys'" \
 		> "~/Desktop/MyCloud_ERROR.txt"
 	exit 0
 }
@@ -24,27 +24,33 @@ mycloud_error()
 # set pipefail
 set -o pipefail
 
-# check if we can ssh in
-ssh "${MYCLOUD}" 'crontab -l' &> /dev/null
-
-if [ $? -ne 0 ]
-then
-	mycloud_error
-fi
-
-# presumabely we can successfully ssh in,
-# now check if the backup job is present
-ENTRY_PRESENT=`ssh "${MYCLOUD}" 'crontab -l' | grep -c '/home/root/backup.sh'`
+# check if the backup job is present
+TMP=/tmp/$$
+ssh "${MYCLOUD}" 'crontab -l' > "${TMP}" 2> /dev/null
 
 if [ $? -eq 0 ]
 then
-	if [ ${ENTRY_PRESENT} -eq 0 ]
+	grep '/home/root/backup.sh' "${TMP}" &> /dev/null
+	EXIT_STATUS=$?
+
+	if [ ${EXIT_STATUS} -eq 1 ]
 	then
 		ssh "${MYCLOUD}" 'printf "%s\n" "4 */3 * * * /home/root/backup.sh &" >> /var/spool/cron/crontabs/root'
+		#printf "it was NOT there\n" 1>&2
+
+	#elif [ ${EXIT_STATUS} -eq 0 ]
+	#then
+	#	printf "it was there\n" 1>&2
+	#else
+	#	printf "grep error\n" 1>&2
 	fi
 else
+	printf "hit this error 1"
 	mycloud_error
 fi
+
+# cleanup
+rm -f "${TMP}"
 
 # set pipefail back to normal
 set +o pipefail
